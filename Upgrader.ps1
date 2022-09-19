bf9b13fa04e006095713ed56b2cd82e0c2010996
OlaYZen <Olaisenvb@gmail.com>
2022-09-19 08:58:20 +0200

    V1.2.2 [BETA]

--- Upgrader.ps1
+++ Upgrader.ps1
@@ -1,8 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-Start-Sleep -Seconds 2
-
-
+Start-Sleep -Milliseconds 500
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
